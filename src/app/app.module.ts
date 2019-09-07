43e0e92a13b65c7256318414ed482f70289e8417
Tom H <tom@codersforlabour.com>
2019-09-07 18:54:16 +0100

    Added inline @import font support
    Will now extract CSS @import statements from SVG CSS, request remote URL, and assemble font-face definitions for each imported font. This is then passed to saveSvgAsPng, allowing final image to honour specified fonts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SvgEditorComponent } from './svg-editor/svg-editor.component';
@@ -14,6 +15,7 @@ import { SvgEditorComponent } from './svg-editor/svg-editor.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     HttpModule
   ],
   providers: [],
